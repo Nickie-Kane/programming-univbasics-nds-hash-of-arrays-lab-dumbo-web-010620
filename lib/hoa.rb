0ebf390bf717e67183f3dcd2b381a56c30a9ae29
nickie kane <kameo712@gmail.com>
2020-01-02 19:59:37 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -8,7 +8,7 @@ BASE_HOA = {
 def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
-  BASE_HOA[:third_earthers] << 'name'
+  BASE_HOA[:third_earthers] << 'Snarf'
 end
 
 add_character(:third_earthers, "Snarf")
